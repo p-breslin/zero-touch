3bfdfa7cb46aa9514983c9b4d6b377c95781d548
Peter <peter.breslin@experienceflow.ai>
2025-05-18 22:47:18 -0700

    script to create duckdb instance

---
+++ dev/create_duckdb.py
@@ -0,0 +1,20 @@
+import os
+import duckdb
+from dotenv import load_dotenv
+from pathlib import Path
+from src.paths import DATA_DIR
+
+load_dotenv()
+db_filename = f"{os.getenv('DB_SUBSET_NAME')}.duckdb"
+savepath = Path(DATA_DIR / db_filename)
+
+# Delete existing file to ensure it's empty
+if os.path.exists(savepath):
+    os.remove(savepath)
+    print(f"Existing '{db_filename}' removed.")
+
+# Connect to create the new database file
+con = duckdb.connect(savepath)
+print(f"Created new DuckDB database: {db_filename}")
+
+con.close()
