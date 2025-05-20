f036104701ddce0014d1f033f4333728c6e977ff
Peter <peter.breslin@experienceflow.ai>
2025-05-19 19:11:39 -0700

    including additional information

--- dev/github_commits.py
+++ dev/github_commits.py
@@ -19,17 +19,23 @@ conn_read = duckdb.connect(database=DUCKDB_READ_PATH, read_only=True)
 
 
 def populate_database(conn_write):
-    """Creates and populates a table for the commit details."""
+    """
+    Creates and populates a table for the commit details. The extracted_issue_key will be inferred by a downstream process.
+    """
     commits_table = f"""
     CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
         commit_sha TEXT PRIMARY KEY,
         commit_timestamp TIMESTAMP,
+        extracted_issue_key TEXT,
         author_id TEXT,
         author_name TEXT,
         author_email TEXT,
         author_login TEXT,
-        commit_message TEXT,
-        extracted_issue_key TEXT,
+        committer_id TEXT,
+        committer_name TEXT,
+        committer_email TEXT,
+        committer_login TEXT,
+        commit_message TEXT
     );
     """
     conn_write.execute(commits_table)
@@ -44,6 +50,7 @@ def get_commits(N=1000):
         "SHA" AS sha,
         "COMMIT" AS commit_blob,
         "AUTHOR" AS author_blob,
+        "COMMITTER" AS committer_blob,
         "COMMIT_TIMESTAMP" AS commit_date
     FROM "{COMPANY_NAME}_GITHUB_"."COMMITS"
     ORDER BY "COMMIT_TIMESTAMP" DESC
@@ -53,38 +60,53 @@ def get_commits(N=1000):
 
     records = []
     for row_tuple in recent_commits:
-        sha, commit_blob_str, author_blob_str, commit_date_ts = row_tuple
+        sha, commit_blob, author_blob, committer_blob, commit_date = row_tuple
 
         try:
             commit_details = (
-                json.loads(commit_blob_str)
-                if isinstance(commit_blob_str, str)
-                else commit_blob_str
+                json.loads(commit_blob) if isinstance(commit_blob, str) else commit_blob
             )
             author_details = (
-                json.loads(author_blob_str)
-                if isinstance(author_blob_str, str)
-                else author_blob_str
+                json.loads(author_blob) if isinstance(author_blob, str) else author_blob
             )
-            author_info = commit_details.get("author", None)
+            committer_details = (
+                json.loads(committer_blob)
+                if isinstance(committer_blob, str)
+                else committer_blob
+            )
+
+            commit_author = commit_details.get("author", None)
+            commit_committer = commit_details.get("committer", None)
 
             record = {
                 "commit_sha": sha,
-                "commit_timestamp": commit_date_ts,
+                "commit_timestamp": commit_date,
+                "extracted_issue_key": None,
                 "author_id": author_details.get("id")
                 if isinstance(author_details, dict)
                 else None,
                 "author_login": author_details.get("login")
                 if isinstance(author_details, dict)
                 else None,
-                "author_name": author_info.get("name")
-                if isinstance(author_info, dict)
+                "author_name": commit_author.get("name")
+                if isinstance(commit_author, dict)
                 else None,
-                "author_email": author_info.get("email")
-                if isinstance(author_info, dict)
+                "author_email": commit_author.get("email")
+                if isinstance(commit_author, dict)
+                else None,
+                "committer_id": committer_details.get("id")
+                if isinstance(committer_details, dict)
+                else None,
+                "committer_login": committer_details.get("login")
+                if isinstance(committer_details, dict)
+                else None,
+                "committer_name": commit_committer.get("name")
+                if isinstance(commit_committer, dict)
+                else None,
+                "committer_email": commit_committer.get("email")
+                if isinstance(commit_committer, dict)
                 else None,
                 "commit_message": commit_details.get("message"),
-                "extracted_issue_key": None,
             }
             records.append(record)
         except Exception as e:
@@ -105,19 +127,23 @@ def get_commits(N=1000):
             (
                 r["commit_sha"],
                 r["commit_timestamp"],
+                r["extracted_issue_key"],
                 r["author_id"],
                 r["author_name"],
                 r["author_email"],
                 r["author_login"],
+                r["committer_id"],
+                r["committer_name"],
+                r["committer_email"],
+                r["committer_login"],
                 r["commit_message"],
-                r["extracted_issue_key"],
             )
             for r in records
         ]
         # ON CONFLICT DO NOTHING to avoid errors if a commit is re-processed
         conn_write.executemany(
             f"""
-            INSERT INTO "{TABLE_NAME}" VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT INTO "{TABLE_NAME}" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             ON CONFLICT (commit_sha) DO NOTHING;
         """,
             insert_data_tuples,
