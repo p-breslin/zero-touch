1711dded50fa2ad01d593b9f953eac9c740e71a8
Peter <peter.breslin@experienceflow.ai>
2025-06-19 22:33:48 -0700

    isolated script for listing all available models

---
+++ frictionless/tests/list_models.py
@@ -0,0 +1,42 @@
+import json
+import config
+import logging
+from utils.logger import setup_logging
+from clients.onboarding_client import OnboardingApiClient
+
+setup_logging()
+log = logging.getLogger(__name__)
+
+
+def fetch_models():
+    """Fetches available models for customer onboarding."""
+    log.info("Initiating model discovery")
+
+    try:
+        # Initialize the client
+        client = OnboardingApiClient(
+            base_url=config.ONBOARDING_API_URL,
+            email=config.ADMIN_EMAIL,
+            password=config.ADMIN_PASSWORD,
+        )
+
+        # Authenticate
+        client.authenticate()
+
+        # Get the list of all available model templates (Industry Categories)
+        models = client.list_industry_categories()
+
+        log.debug("\n--- Available Industry Categories (Model Templates) ---")
+        log.debug(json.dumps(models, indent=2))
+
+        model_tuples = {}
+        for model in models:
+            model_tuples[model["id"]] = model["name"]
+        print(json.dumps(model_tuples, indent=2))
+
+    except Exception as e:
+        log.error(f"An error occurred during the model discovery process: {e}")
+
+
+if __name__ == "__main__":
+    fetch_models()
