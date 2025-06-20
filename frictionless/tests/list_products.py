8ade19481f7f7cd9343d8ed7529733f06ebb3bcc
Peter <peter.breslin@experienceflow.ai>
2025-06-19 22:33:56 -0700

    isolated script for listing all available products

---
+++ frictionless/tests/list_products.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import config
+import logging
+from utils.logger import setup_logging
+from clients.onboarding_client import OnboardingApiClient
+
+setup_logging()
+log = logging.getLogger(__name__)
+
+
+def fetch_products():
+    """Lists the available Products."""
+
+    customer_email = config.NEW_CUSTOMER_PAYLOAD["email"]
+    client = OnboardingApiClient(
+        base_url=config.ONBOARDING_API_URL,
+        email=config.ADMIN_EMAIL,
+        password=config.ADMIN_PASSWORD,
+    )
+
+    try:
+        # Authenticate as Partner
+        log.info("Authenticating partner to generate a customer token..")
+        client.authenticate()
+
+        # Generate a token scoped to the newly created customer
+        log.info(f"Generating token for customer: {customer_email}")
+        customer_token = client.generate_customer_token(customer_email)
+        log.info("Successfully generated customer-scoped token.")
+
+        # List available products
+        available_products = client.list_products(customer_token)
+        log.info("Available Products:\n" + json.dumps(available_products, indent=2))
+
+    except Exception as e:
+        log.error(f"Error during database initialization flow: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    fetch_products()
